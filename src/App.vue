9fc5828029debeb6e624c3dbace86c0fba06843c
mathi <mathi@gmail.com>
2022-08-29 23:46:17 -0900

    this is vue.js tutorial for begineers

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
+<div id="app">
+<CalCulator></CalCulator>
+</div>
+  </template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalCulator from './components/CalCulator.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    CalCulator
+}
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: #31475e !important;
 }
 </style>
---
+++ src/components/CalCulator.vue
@@ -0,0 +1,71 @@
+<template>
+  <!--heading-->
+  <div class="p-4" style="max-width:400px;margin:25px auto;background:#123">
+<!--cal reslt-->
+<div class="bg-vue-dark lead w-full rounded m-1 p-3 text-right font-weight-bold text-white">
+  {{calculatorValue || 0}}
+</div>
+<!--cal button-->
+<div class="row ">
+<div class="col-3" v-for="n in calculatorElement" :key="n">
+<div class="lead text-white text-center m-1 py-3 bg-vue-dark rounded hover-class"
+:class="{'bg-vue-green':['C','*','/','-','+','%','='].includes(n)}"
+@click="action(n)">
+{{n}}
+</div>
+</div>
+</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CalCulator',
+  data(){
+    return{
+      calculatorValue:'',
+      calculatorElement:['C','*','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
+      operator:null,
+      previousCalculatorValue:'',
+    }
+  },
+  methods:{
+    action(n){
+      if(!isNaN(n) || n === '.'){
+        this.calculatorValue+=n+ '';
+      }
+      if(n==='C'){
+        this.calculatorValue='';
+      }
+      if(n==='%'){
+        this.calculatorValue=this.calculatorValue/100+'';
+      }
+      if(['/','*','-','+'].includes(n)){
+        this.operator=n;
+        this.previousCalculatorValue=this.calculatorValue;
+        this.calculatorValue='';
+      }
+      if(n==='='){
+        this.calculatorValue=eval(
+          this.previousCalculatorValue+this.operator+this.calculatorValue
+        );
+        this.previousCalculatorValue='';
+        this.operator=null;
+      }
+    }
+  }
+ 
+}
+</script>
+
+<style scoped>
+.bg-vue-dark{
+  background: #31475e;
+}
+.hover-class:hover{
+  cursor: pointer;
+  background:  #3D5875;
+}
+.bg-vue-green{
+  background: #3fb984;
+}
+</style>
